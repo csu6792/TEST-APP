626318289a560515305f13692a8feab47eaf34f6
csu6792 <53500308+csu6792@users.noreply.github.com>
2026-05-14 11:45:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ async function detect(img) {
 
   console.log(outputs);
 
-  drawDemoBox();
+  drawBoxes(outputs.output0.cpuData);
 
 }
 
@@ -86,16 +86,46 @@ function preprocessing(data, size) {
   return float32Data;
 }
 
-function drawDemoBox() {
+function drawBoxes(data) {
 
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 3;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.strokeRect(
-    100,
-    100,
-    200,
-    200
-  );
+  const scaleX = image.width / 640;
+  const scaleY = image.height / 640;
+
+  for (let i = 0; i < data.length; i += 6) {
+
+    const x1 = data[i];
+    const y1 = data[i + 1];
+    const x2 = data[i + 2];
+    const y2 = data[i + 3];
+
+    const score = data[i + 4];
+    const classId = data[i + 5];
+
+    if (score < 0.5) continue;
+
+    const w = x2 - x1;
+    const h = y2 - y1;
+
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 3;
+
+    ctx.strokeRect(
+      x1 * scaleX,
+      y1 * scaleY,
+      w * scaleX,
+      h * scaleY
+    );
+
+    ctx.fillStyle = "red";
+
+    ctx.fillText(
+      `ID:${classId} ${score.toFixed(2)}`,
+      x1 * scaleX,
+      y1 * scaleY - 5
+    );
+
+  }
 
 }
